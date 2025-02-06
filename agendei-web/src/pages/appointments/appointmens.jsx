c4fb9b2742c87bccbf82a40f31097b6df5087e6b
icaro armando <icaroarmando21@gmail.com>
2025-02-06 14:49:16 -0300

    adding filter and buadding filter and button

--- agendei-web/src/pages/appointments/appointmens.jsx
+++ agendei-web/src/pages/appointments/appointmens.jsx
@@ -1,10 +1,26 @@
-import Navbar from "../../components/navbar/navbar.jsx"; 
+import Navbar from "../../components/navbar/navbar.jsx";
+import { Link } from "react-router-dom";
 
 function Appointmens() {
-    return <>
-    <Navbar/>
-        <h1>Agendamentos</h1>
-    </>
+    return <div className="container-fluid mt-page">
+        <Navbar />
+
+        <div className="d-flex justify-content-between align-items-center">
+            <div className="">
+                <h2 className="d-inline">Agendamentos</h2>
+                <Link to="/appointments/add" className="btn btn-outline-primary ms-5">
+                    Novo Agendamento
+                </Link>
+            </div>
+
+            <div className="d-flex  justify-content-end">
+                <input id="startDate" type="date" className="form-control" />
+                <span>Até</span>
+                <input id="endDate" type="date" className="form-control" />
+            </div>
+
+        </div>
+    </div>
 }
 
 export default Appointmens;
