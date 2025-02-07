6b42b7a16aae8092f51169adc25ae961cec3dce3
icaro armando <icaroarmando21@gmail.com>
2025-02-07 16:32:56 -0300

    fix(login): fix error in API export and adjust authentication logic - Fixed API instance export - Adjusted the display of error messages upon login - Improved token storage in localStorage

--- agendei-web/src/pages/login/login.jsx
+++ agendei-web/src/pages/login/login.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom"
 import "./login.css"
 import logo from "../../assets/logo.png"
 import fundo from "../../assets/fundo.png"
-import { api } from "../../constants/api.js";
+import api from "../../constants/api.js";
 
 function Login() {
 
@@ -11,9 +11,37 @@ function Login() {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [msg, setMsg] = useState("");
 
-    function ExecuteLogin(){
-        navigate("/appointments");
+    async function ExecuteLogin() {
+
+        setMsg("");
+
+        try {
+            const response = await api.post("/users/login", {
+                email,
+                password
+            });
+
+            if (response.data) {
+                localStorage.setItem("sessionToken", response.data.token);
+                localStorage.setItem("sessionUserId", response.data.id_user);
+                localStorage.setItem("sessionUserEmail", response.data.email);
+                localStorage.setItem("sessionUserName", response.data.name);
+                navigate("/appointments");
+                
+            } else {
+                setMsg("Erro ao efetuar login");
+            }
+            // navigate("/appointments");
+        }
+        catch (error) {
+            if (error.response?.data.error)
+                setMsg(error.response?.data.error);
+            else
+                setMsg("Erro ao efetuar login");
+            // console.log(error);
+        }
     }
 
 
@@ -25,16 +53,24 @@ function Login() {
                 <h5 className="mb-4 text-secondary">Acesse sua conta </h5>
 
                 <div className="mt-4">
-                    <input type="email" placeholder="E-mail" 
-                    className="form-control" onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="email" placeholder="E-mail"
+                        className="form-control" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="mt-2">
-                    <input type="password" placeholder="Senha" 
-                    className="form-control" onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" placeholder="Senha"
+                        className="form-control" onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className="mt-3 mb-5">
                     <button onClick={ExecuteLogin} className="btn btn-primary w-100" type="button">Entrar</button>
                 </div>
+
+                {
+                    msg.length > 0 &&
+                    <div className="alert alert-danger" role="alert">
+                        {msg}
+                    </div>
+                }
+
 
                 <div >
                     <span className="me-1">Não tenho uma conta.</span>
@@ -44,7 +80,7 @@ function Login() {
         </div>
 
         <div className="col-sm-7">
-            <img src={fundo} className="background-login"/>
+            <img src={fundo} className="background-login" />
         </div>
 
     </div>
