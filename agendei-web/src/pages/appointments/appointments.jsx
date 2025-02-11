5cfdf2bc16e1d6f155b635da344e1340d90eb1f5
icaro armando <icaroarmando21@gmail.com>
2025-02-11 11:45:52 -0300

    feat: load doctors and appointments via API and add dynamic doctor and period filters

--- agendei-web/src/pages/appointments/appointments.jsx
+++ agendei-web/src/pages/appointments/appointments.jsx
@@ -1,14 +1,19 @@
 import "./appointments.css";
 import Navbar from "../../components/navbar/navbar.jsx";;
 import { Link, useNavigate } from "react-router-dom";
-import { doctors, appointments } from "../../constants/data.js";
 import Appointment from "../../components/appointment/appointment.jsx";
-import { use, useEffect } from "react";
+import { useEffect, useState } from "react";
 import api from "../../constants/api.js";
 
 function Appointments() {
 
     const navigate = useNavigate();
+    const [appointments, setAppointments] = useState([]);
+    const [doctors, setDoctors] = useState([]);
+
+    const [idDoctor, setIdDoctor] = useState("");
+    const [dtStart, setDtStart] = useState("");
+    const [dtEnd, setDtEnd] = useState("");2024
 
     function ClickEdit(id_appointment) {
         navigate("/appointments/edit/" + id_appointment);
@@ -18,98 +23,137 @@ function Appointments() {
         console.log("Excluir " + id_appointment);
     }
 
-    async function loadAppointments() {
+    async function LoadDoctors() {
+
         try {
-            const response = await api.get("/admin/appointments", {
-
-                params: {id_doctor: 4}
-
-            });
+            const response = await api.get("/doctors");
 
             if (response.data) {
+                setDoctors(response.data);
             }
+
         } catch (error) {
-            if (error.response?.data.error)
+            if (error.response?.data.error) {
+                if (error.response.status == 401)
+                    return navigate("/");
+
                 alert(error.response?.data.error);
+            }
             else
-                alert("Erro ao efetuar login");
+                alert("Erro ao listar médicos.");
         }
     }
 
-            useEffect(() => {
-                loadAppointments();
-            }, []);
+    async function LoadAppointments() {
+        console.log("LoadAppointments...");
 
-            return <div className="container-fluid mt-page">
-                <Navbar />
+        try {
+            const response = await api.get("/admin/appointments", {
+                params: {
+                    id_doctor: idDoctor,
+                    dt_start: dtStart,
+                    dt_end: dtEnd
+                }
+            });
 
-                <div className="d-flex justify-content-between align-items-center">
-                    <div>
-                        <h2 className="d-inline">Agendamentos</h2>
-                        <Link to="/appointments/add"
-                            className="btn btn-outline-primary ms-5 mb-2">
-                            Novo Agendamento
-                        </Link>
-                    </div>
+            if (response.data) {
+                setAppointments(response.data);
+            }
 
-                    <div className="d-flex justify-content-end">
-                        <input id="startDate" className="form-control" type="date" />
-                        <span className="m-2">Até</span>
-                        <input id="endtDate" className="form-control" type="date" />
+        } catch (error) {
+            if (error.response?.data.error) {
 
-                        <div className="form-control ms-3 me-3">
-                            <select name="doctor" id="doctor">
-                                <option value="">Todos os médicos</option>
+                if (error.response.status == 401)
+                    return navigate("/");
 
-                                {
-                                    doctors.map((doc) => {
-                                        return <option key={doc.id_doctor}
-                                            value={doc.id_doctor}>
-                                            {doc.name}
-                                        </option>
-                                    })
-                                }
-                            </select>
-                        </div>
-
-                        <button onClick={loadAppointments} type="button" className="btn btn-primary">Filtrar</button>
-                    </div>
-
-                </div>
-
-                <div>
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">Paciente</th>
-                                <th scope="col">Médico</th>
-                                <th scope="col">Serviço</th>
-                                <th scope="col">Data/Hora</th>
-                                <th scope="col" className="text-end">Valor</th>
-                                <th scope="col" className="col-buttons"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                appointments.map((ap) => {
-                                    return <Appointment key={ap.id_appointment}
-                                        id_appointment={ap.id_appointment}
-                                        user={ap.user}
-                                        doctor={ap.doctor}
-                                        service={ap.service}
-                                        booking_date={ap.booking_date}
-                                        booking_hour={ap.booking_hour}
-                                        price={ap.price}
-                                        clickEdit={ClickEdit}
-                                        clickDelete={ClickDelete}
-                                    />
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </div>
-
-            </div>
+                alert(error.response?.data.error);
+            }
+            else
+                alert("Erro ao efetutar login. Tente novamente mais tarde.");
         }
+    }
+
+    function ChangeDoctor(e) {
+        setIdDoctor(e.target.value);
+    }
+
+    useEffect(() => {
+        LoadDoctors();
+        LoadAppointments();
+    }, []);
+
+    return <div className="container-fluid mt-page">
+        <Navbar />
+
+        <div className="d-flex justify-content-between align-items-center">
+            <div>
+                <h2 className="d-inline">Agendamentos</h2>
+                <Link to="/appointments/add"
+                    className="btn btn-outline-primary ms-5 mb-2">
+                    Novo Agendamento
+                </Link>
+            </div>
+
+            <div className="d-flex justify-content-end">
+                <input id="startDate" className="form-control" type="date"
+                    onChange={(e) => setDtStart(e.target.value)} />
+                <span className="m-2">Até</span>
+                <input id="endtDate" className="form-control" type="date"
+                    onChange={(e) => setDtEnd(e.target.value)} />
+
+                <div className="form-control ms-3 me-3">
+                    <select name="doctor" id="doctor" value={idDoctor} onChange={ChangeDoctor}>
+                        <option value="">Todos os médicos</option>
+
+                        {
+                            doctors.map((doc) => {
+                                return <option key={doc.id_doctor}
+                                    value={doc.id_doctor}>
+                                    {doc.name}
+                                </option>
+                            })
+                        }
+                    </select>
+                </div>
+
+                <button onClick={LoadAppointments} className="btn btn-primary" type="button">Filtrar</button>
+            </div>
+
+        </div>
+
+        <div>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Paciente</th>
+                        <th scope="col">Médico</th>
+                        <th scope="col">Serviço</th>
+                        <th scope="col">Data/Hora</th>
+                        <th scope="col" className="text-end">Valor</th>
+                        <th scope="col" className="col-buttons"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        appointments.map((ap) => {
+                            return <Appointment key={ap.id_appointment}
+                                id_appointment={ap.id_appointment}
+                                user={ap.user}
+                                doctor={ap.doctor}
+                                service={ap.service}
+                                booking_date={ap.booking_date}
+                                booking_hour={ap.booking_hour}
+                                price={ap.price}
+                                clickEdit={ClickEdit}
+                                clickDelete={ClickDelete}
+                            />
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+}
 
 export default Appointments;
